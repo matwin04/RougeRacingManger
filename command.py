fc0221ca5f277f702719ba3ca7572df28061b84a
thememetsar <mattwiner04@gmail.com>
2022-12-26 19:12:46 -0800

    26-I

--- command.py
+++ command.py
@@ -1,60 +1,54 @@
-import socket
-import os
 from appJar import gui
+import webbrowser
+import time
+import pyautogui
+import subprocess
+import os
 
-app = gui()
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print('Starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
-
-# Listen for incoming connections
-sock.listen(1)
-
+def press(button):
+    if button == "OpenWeb":
+        app.open_web("http://www.google.com")
+    if button == "Run Sigma":
+        pyautogui.hotkey('win')
+        pyautogui.sleep(1)
+        pyautogui.typewrite('sigma simulation')
+        pyautogui.press('enter')
+    if button == "New Race LB":
+        pyautogui.sleep(1)
+        pyautogui.typewrite('https://192.168.1.12:6001/RemoteViewSettings')
+        pyautogui.press('enter')
+    if button == "Mute":
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "toggle"])
+    elif button == "Lower":
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "5%-"])
+    elif button == "Raise":
+        subprocess.run(["amixer", "-D", "pulse", "sset", "Master", "5%+"])
 def restart(btn):
-    # Restart the computer
-    pass
+    if btn == "Restart":
+        os.system("shutdown /r /t 0")
+    if btn == "Shut Down":
+        os.system("shutdown /s /t 1")
+    if btn == "Sleep":
+        subprocess.run(["rundll32.exe", "powrprof.dll,SetSuspendState"])
+        
+app = gui("RRM", "400x200")
+app.startTabbedFrame("MainMenu") 
+app.startTab("MainMenu")  
+time_label = app.addLabel("time", "")
+time_label.config(font=("Arial", 20))
+def update_time():
+    current_time = time.strftime("%I:%M:%S %p")
+    time_label.config(text=current_time)
+    app.after(1000, update_time)
+update_time() 
 
-def sleep(btn):
-    # Put the computer to sleep
-    os.system("rundll32.exe powrprof.dll,SetSuspendState Sleep")
+app.startTab("ControlPannel")
+app.button("Run Sigma", press)
+app.button("New Race LB", press)
+app.addButtons(["Restart","Shut Down","Sleep"], restart)
+app.addLabel("l1", "Volume Control")
+app.addButtons(["Mute", "Lower", "Raise"], press)
+app.stopTab()
 
 
-def shutdown(btn):
-    # Shut down the computer
-    pass
-# Create the GUI
-app.addButtons(['Restart', 'Sleep', 'Shutdown'], [restart, sleep, shutdown])
 app.go()
-
-while True:
-    # Wait for a connection
-    print('Waiting for a connection')
-    connection, client_address = sock.accept()
-    try:
-        print('Connection from', client_address)
-
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            if data:
-                command = data.decode()
-                # Execute the command here
-                print('Received "{}"'.format(command))
-                if command == 'restart':
-                    restart(None)
-                elif command == 'sleep':
-                    sleep(None)
-                elif command == 'shutdown':
-                    shutdown(None)
-            else:
-                print('No more data from', client_address)
-                break
-            
-    finally:
-        # Clean up the connection
-        connection.close()
